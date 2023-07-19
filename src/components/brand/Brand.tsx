3aa666451238ae06a81fec1edfe09ce4605f4860
Lone <adeyanju.akorede1@gmail.com>
2023-07-19 08:02:39 +0100

    feat: brand section

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./brand.css";
+import {
+  AtlassianLogoPNG,
+  DropBoxLogoPNG,
+  GoogleLogoPNG,
+  ShopifyLogoPNG,
+  SlackLogoPNG,
+} from "../../constants/image_assets";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={GoogleLogoPNG} alt="google logo" />
+      </div>
+      <div>
+        <img src={SlackLogoPNG} alt="slack logo" />
+      </div>
+      <div>
+        <img src={AtlassianLogoPNG} alt="atlassian logo" />
+      </div>
+      <div>
+        <img src={DropBoxLogoPNG} alt="dropbox logo" />
+      </div>
+      <div>
+        <img src={ShopifyLogoPNG} alt="shopify logo" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
--- src/constants/image_assets.ts
+++ src/constants/image_assets.ts
@@ -1,9 +1,21 @@
-import LogoSVG from '../assets/images/logo.svg';
+import LogoSVG from "../assets/images/logo.svg";
 import PeoplePNG from "../assets/images/people.png";
 import AIHeadPNG from "../assets/images/ai_head.png";
+import AtlassianLogoPNG from "../assets/images/atlassian.png";
+import DropBoxLogoPNG from "../assets/images/dropbox.png";
+import GoogleLogoPNG from "../assets/images/google.png";
+import SlackLogoPNG from "../assets/images/slack.png";
+import ShopifyLogoPNG from "../assets/images/shopify.png";
+import PossibilityImagePNG from "../assets/images/possibility.png";
 
 export {
   LogoSVG,
-  PeoplePNG ,
-  AIHeadPNG
+  PeoplePNG,
+  AIHeadPNG,
+  AtlassianLogoPNG,
+  DropBoxLogoPNG,
+  GoogleLogoPNG,
+  SlackLogoPNG,
+  ShopifyLogoPNG,
+  PossibilityImagePNG,
 };
