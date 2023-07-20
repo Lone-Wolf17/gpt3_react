47c575a2686f89147ac7206c469c3d8de8f94e36
Lone <adeyanju.akorede1@gmail.com>
2023-07-20 10:10:01 +0100

    feat: blog section

--- src/constants/image_assets.ts
+++ src/constants/image_assets.ts
@@ -1,4 +1,4 @@
-import LogoSVG from "../assets/images/logo.svg";
+import Gpt3LogoSVG from "../assets/images/logo.svg";
 import PeoplePNG from "../assets/images/people.png";
 import AIHeadPNG from "../assets/images/ai_head.png";
 import AtlassianLogoPNG from "../assets/images/atlassian.png";
@@ -7,15 +7,25 @@ import GoogleLogoPNG from "../assets/images/google.png";
 import SlackLogoPNG from "../assets/images/slack.png";
 import ShopifyLogoPNG from "../assets/images/shopify.png";
 import PossibilityImagePNG from "../assets/images/possibility.png";
+import Blog01Image from "../assets/images/blog-01.png";
+import Blog02Image from "../assets/images/blog-02.png";
+import Blog03Image from "../assets/images/blog-03.png";
+import Blog04Image from "../assets/images/blog-04.png";
+import Blog05Image from "../assets/images/blog-05.png";
 
 export {
-  LogoSVG,
-  PeoplePNG,
   AIHeadPNG,
   AtlassianLogoPNG,
+  Blog01Image,
+  Blog02Image,
+  Blog03Image,
+  Blog04Image,
+  Blog05Image,
   DropBoxLogoPNG,
   GoogleLogoPNG,
+  Gpt3LogoSVG,
+  PeoplePNG,
+  PossibilityImagePNG,
   SlackLogoPNG,
   ShopifyLogoPNG,
-  PossibilityImagePNG,
 };
--- src/containers/blog/Blog.tsx
+++ src/containers/blog/Blog.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React from "react";
 import "./blog.css";
+import { Article } from "../../components";
+import { Blog01Image, Blog02Image, Blog03Image, Blog04Image, Blog05Image } from "../../constants/image_assets";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3_blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, <br /> We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={Blog01Image}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imgUrl={Blog02Image}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={Blog03Image}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={Blog04Image}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={Blog05Image}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
